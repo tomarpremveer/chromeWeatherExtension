0bfba95f9d91dd37d8ed945551e1e8196edf74a2
tomarpremveer <montytomar78@gmail.com>
2021-01-07 14:41:26 +0530

    files updated

---
+++ background.js
@@ -0,0 +1,7 @@
+function getLocation() {
+  //   navigator.geolocation.getCurrentPosition((position) => {
+  //     alert(position.coords.latitude);
+  //   });
+  // document.querySelector("#weather").innerHTML = "hi this is from script tag";
+}
+document.addEventListener("load", getLocation);
