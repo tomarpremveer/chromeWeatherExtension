ab0f0619eead0ba4d934e961802dc5397f8179dd
tomarpremveer <montytomar78@gmail.com>
2021-01-08 21:53:14 +0530

    images added

--- background.js
+++ background.js
@@ -1,7 +1,16 @@
 chrome.runtime.onInstalled.addListener(function () {
   navigator.geolocation.getCurrentPosition((position) => {
-    chrome.storage.sync.set({ location: location }, function () {
-      console.log("location is defined");
-    });
+    console.log(position.coords.latitude, position.coords.longitude);
+    chrome.storage.sync.set(
+      {
+        location: {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        },
+      },
+      function () {
+        console.log("location is defined");
+      }
+    );
   });
 });
