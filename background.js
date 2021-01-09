044ca3a781f7d1b1fc5bec339c9c0d9e6ccdf538
tomarpremveer <montytomar78@gmail.com>
2021-01-09 11:04:24 +0530

    accuracy option added to geolocation method

--- background.js
+++ background.js
@@ -1,16 +1,22 @@
 chrome.runtime.onInstalled.addListener(function () {
-  navigator.geolocation.getCurrentPosition((position) => {
-    console.log(position.coords.latitude, position.coords.longitude);
-    chrome.storage.sync.set(
-      {
-        location: {
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
+  navigator.geolocation.getCurrentPosition(
+    (position) => {
+      console.log(position.coords.latitude, position.coords.longitude);
+      chrome.storage.sync.set(
+        {
+          location: {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+          },
         },
-      },
-      function () {
-        console.log("location is defined");
-      }
-    );
-  });
+        function () {
+          console.log("location is defined");
+        }
+      );
+    },
+    (err) => {
+      console.log(err);
+    },
+    { maximumAge: 10000, enableHighAccuracy: true }
+  );
 });
